1f61d62bef2c958dc6c63bafe8030f55794edbd3
martinhao <robmartinhao@gmail.com>
2024-03-04 16:38:43 -0300

    Service discovery e registry

--- src/main/java/br/com/alurafood/server/ServerApplication.java
+++ src/main/java/br/com/alurafood/server/ServerApplication.java
@@ -2,7 +2,9 @@ package br.com.alurafood.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServerApplication {
 
